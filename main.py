c9cd8f507b560a917db9cf52233dfc6ec6f13c5f
Ubuntu <bludrimjr@gmail.com>
2018-07-09 16:48:55 +0000

    Implement /register

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from flask import Flask, request, make_response, redirect
+from flask import Flask, request, make_response, redirect, jsonify
+import bson
 from flask_pymongo import PyMongo as pymongo
 import json
 
@@ -10,12 +11,33 @@ mongo = pymongo(app)
 def hello():
     return "Hello world!"
 
-@app.route("/register/", methods=['POST', 'GET'])
+@app.route("/register/", methods=['POST'])
 def register():
-    if request.method == 'POST':
-        pass
-    else:
-        pass
+    # Retrieve JSON from POST
+    reg_json = request.get_json(silent=True)
+    if (reg_json is None):
+        return make_response("JSON should be given", 400)
+    
+    if not "token" in reg_json.keys():
+        return make_response("JSON should contain token", 400)
+
+    # insert new user info if it does not duplicate
+    cursor = mongo.db.accounts.find({"token" : reg_json["token"]})
+    if cursor.count() > 0:
+        return make_response("Given token is already registered", 400)
+
+    mongo.db.accounts.insert_one(reg_json)
+
+    # return all other user's info.
+    res_array = []
+    cursor = mongo.db.accounts.find({"token" : {"$ne" : reg_json["token"]}}) # contains _id too.
+    for account_info in cursor:
+        _id = account_info["_id"]
+        assert type(_id) == bson.objectid.ObjectId
+        account_info["_id"] = str(_id)
+        res_array.append(account_info)
+
+    return jsonify(res_array)
 
 
 @app.route("/login/", methods=['POST', 'GET'])
@@ -35,11 +57,9 @@ def login():
                 Exception("Token %s exists more than once" % token)
             return make_response("Token already exists", 200)
         else:
-            #return redirect("http://qppepdp.koreacentral.cloudapp.azure.com:8000/register/", code=302)
             res = make_response("register please")
             res.mimetype = "text/plain"
             return res
-            #mongo.db.accounts.insert_one({"token" : token})
 
         return make_response("I got your POST", 200)
     else:
@@ -56,13 +76,12 @@ def login():
                 Exception("Token %s exists more than once" % token)
             return make_response("Token already exists", 200)
         else:
-            #return redirect("http://qppepdp.koreacentral.cloudapp.azure.com:8000/register/", code=302)
-            #mongo.db.accounts.insert_one({"token" : token})
             res = make_response("register please")
             res.mimetype = "text/plain"
             return res
 
         return make_response("I got your GET", 200)
+
 
 if (__name__ == '__main__'):
     app.run(host='0.0.0.0', port = 8000, debug=True)
